52c76b657669f9f871c0e8588c806c96585c26e3
UNKNWNCLOWNGHL <58516721+H04X4@users.noreply.github.com>
2025-08-20 01:23:34 +0400

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-rqf8an2zqm4qs#f0s2(r&ipptc7b4qfn*_4!0i)@)381shrmos'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
